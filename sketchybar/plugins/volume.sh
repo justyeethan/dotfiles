c47a861df87d38df76d4a70868a99fe97d0671d3
Ethan <diamondspectreme@gmail.com>
2024-05-05 19:13:57 -0700

    kitty changes

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -3,25 +3,31 @@
 # Get current volume level
 volume=$(osascript -e 'output volume of (get volume settings)')
 case $volume in
-  9[0-9]|100) ICON="􀊩"
-    PADDING_LEFT=3
-    PADDING_RIGHT=6
-  ;;
-  [6-8][0-9]) ICON="􀊧"
-    PADDING_LEFT=5
-    PADDING_RIGHT=7
-  ;;
-  [3-5][0-9]) ICON="􀊥"
-    PADDING_LEFT=6
-    PADDING_RIGHT=10
-  ;;
-  [1-2][0-9]) ICON="􀊡"
-    PADDING_LEFT=7
-    PADDING_RIGHT=14
-  ;;
-  *) ICON="􀊣"
-    PADDING_LEFT=7
-    PADDING_RIGHT=8
+9[0-9] | 100)
+	ICON="􀊩"
+	PADDING_LEFT=3
+	PADDING_RIGHT=6
+	;;
+[6-8][0-9])
+	ICON="􀊧"
+	PADDING_LEFT=5
+	PADDING_RIGHT=7
+	;;
+[3-5][0-9])
+	ICON="􀊥"
+	PADDING_LEFT=6
+	PADDING_RIGHT=10
+	;;
+[1-2][0-9])
+	ICON="􀊡"
+	PADDING_LEFT=7
+	PADDING_RIGHT=14
+	;;
+*)
+	ICON="􀊣"
+	PADDING_LEFT=7
+	PADDING_RIGHT=8
+	;;
 esac
 
 # icon.padding_right=7\
