07f4bb52e117cd9616da370746463d8552c084a4
Ethan <diamondspectreme@gmail.com>
2025-04-28 18:55:18 -0700

    new sketchybar

--- sketchybar/README.md
+++
@@ -1,5 +0,0 @@
-# Sketchybar Setup
-
-First, you'll need to edit some of the plugins to make sure it works with the VPN you're using. 
-
-You can re-write a bunch of the logic, but essentially I'm just grepping the output of ifconfig to determine if ipsec0 exists in ifconfig, which is how NordVPN's manual installation logic creates. I've also sourced the IP address of NordVPN's IP address that it creates in ifconfig, and determines if that exists. If you're using NordVPN as well, you'll need to create a file in the $CONFIG_DIR (the base sketchybar directory) and call it VPN_IP.
--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,71 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-source "$CONFIG_DIR/colors.sh"
-TMP="/tmp/drawing_state.txt"
+PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-render_item() {
-	PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-	CHARGING=$(pmset -g batt | grep 'AC Power')
-	COLOR=$ICON_COLOR
-	local DRAWING="on"
+if [ "$PERCENTAGE" = "" ]; then
+  exit 0
+fi
 
-	if [ $PERCENTAGE = "" ]; then
-		exit 0
-	fi
-
-	case ${PERCENTAGE} in
-	9[0-9] | 100)
-		ICON="􀛨"
-		;;
-	[6-8][0-9])
-		ICON="􀺸"
-		;;
-	[3-5][0-9])
-		ICON="􀺶"
-		;;
-	[1-2][0-9])
-		ICON="􀛩"
-		COLOR=$(getcolor yellow)
-		DRAWING="on"
-		;;
-	*)
-		ICON="􀛪"
-		COLOR=$(getcolor red)
-		DRAWING="on"
-		;;
-	esac
-
-	if [[ $CHARGING != "" ]]; then
-		ICON="􀢋"
-	fi
-
-	sketchybar --set $NAME icon=$ICON icon.color=$COLOR label=$PERCENTAGE% label.color=$LABEL_COLOR label.drawing=$DRAWING label.font.size=15.0
-}
-
-save_label_state() {
-	echo "$(sketchybar --query $NAME | jq -r '.label.drawing')" >"$TMP"
-}
-
-get_label_state() {
-	if [ -e "$TMP" ]; then
-		cat "$TMP"
-	else
-		echo "off" >"$TMP"
-	fi
-}
-
-label_toggle() {
-	DRAWING="on"
-
-	sketchybar --set $NAME label.drawing=$DRAWING center
-	save_label_state
-}
-
-case "$SENDER" in
-"mouse.clicked")
-	label_toggle
-	;;
-"routine" | "forced" | "power_source_change")
-	render_item
-	;;
+case "${PERCENTAGE}" in
+  9[0-9]|100) ICON=""
+  ;;
+  [6-8][0-9]) ICON=""
+  ;;
+  [3-5][0-9]) ICON=""
+  ;;
+  [1-2][0-9]) ICON=""
+  ;;
+  *) ICON=""
 esac
+
+if [[ "$CHARGING" != "" ]]; then
+  ICON=""
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
